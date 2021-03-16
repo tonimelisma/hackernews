c0ebfdbc06545472dc1637e4a043ad0eebba98e8
Toni Melisma <toni.melisma@iki.fi>
2021-03-16 22:56:03 +0200

    Increase limits

--- util/config.js
+++ util/config.js
@@ -8,9 +8,9 @@ const DB_URI =
 const DB_URI_CLOUD = process.env.DB_URI_CLOUD;
 
 const minDayScore = 10; // a bit over 100 stories a day
-const minWeekScore = 50; // 40; // over 300 stories a week over
-const minMonthScore = 200; // 100; // over 300 stories a month over
-const minYearScore = 500;
+const minWeekScore = 100; // 40; // over 300 stories a week over
+const minMonthScore = 500; // 100; // over 300 stories a month over
+const minYearScore = 1000;
 
 const limitResults = 500;
 
