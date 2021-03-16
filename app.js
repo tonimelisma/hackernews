e69bf415516e1f192a3c1337027e20fac79f8c83
Toni Melisma <toni.melisma@iki.fi>
2021-03-16 23:00:42 +0200

    Update packages

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const logger = require("morgan");
 
 const apiRouter = require("./routes/api");
@@ -15,8 +14,8 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // STATIC
